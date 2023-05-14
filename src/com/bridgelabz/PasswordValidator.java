11f17900a0068cd190c621d1f4c0c0d6b335c81a
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-15 02:08:45 +0530

    Added:As a User need to follow pre -defined Password rules. Rule1 – minimum 8 Characters - and atleast one upper caseNOTE – All rules must

--- src/com/bridgelabz/PasswordValidator.java
+++ src/com/bridgelabz/PasswordValidator.java
@@ -2,9 +2,15 @@ package com.bridgelabz;
 public class PasswordValidator {
     public static boolean isPasswordValid(String password) {
         // Check if the password length is at least 8 characters
-        return password.length() >= 8;
+        if (password.length() < 8) {
+            return false;
+        }
+
+        // Check if the password contains at least one uppercase letter
+        return password.matches(".*[A-Z].*");
     }
 }
+
 
 
 
