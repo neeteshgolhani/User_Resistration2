6bf75875603c39892822501620ebf02c56b63391
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-15 01:20:48 +0530

    Added:As a User need to enter a valid Email

--- src/com/bridgelabz/Validator.java
+++ src/com/bridgelabz/Validator.java
@@ -1,11 +1,11 @@
 package com.bridgelabz;
-import java.util.regex.Pattern;
 public class Validator {
-    public static boolean isValidName(String firstName, String lastName) {
-        String namePattern = "^[A-Z][a-zA-Z]{2,}$";
+     public static boolean isValidEmail(String email) {
+         // Regular expression pattern to match the required conditions
+         String emailPattern = "^[A-Za-z0-9]+([._%+-][A-Za-z0-9]+)*@[A-Za-z0-9]+(\\.[A-Za-z]{2,}){1,2}$";
 
-        return firstName.matches(namePattern) && lastName.matches(namePattern);
-    }
+         return email.matches(emailPattern); // Check if the email address matches the pattern
+     }
 }
 
 
